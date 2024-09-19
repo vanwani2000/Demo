303e555ea3ebde0978b9c62f2110d660e7bf8ffe
vanwani2000 <rajavanwani2001@gmail.com>
2024-09-19 00:22:07 -0700

    feat(BinaryTreeNode.cpp): Add BinaryTreeNode code

---
+++ BinaryTreeNode.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T> class BinaryTreeNode {
+public:
+  T data;
+  BinaryTreeNode<T> *left;
+  BinaryTreeNode<T> *right;
+
+  BinaryTreeNode(T data) {
+    this->data = data;
+    left = NULL;
+    right = NULL;
+  }
+
+  ~BinaryTreeNode() {
+    if (left)
+      delete left;
+    if (right)
+      delete right;
+  }
+};
+
+BinaryTreeNode<int> *takeInput() {
+  int rootData;
+  cin >> rootData;
+
+  if (rootData == -1) {
+    return NULL;
+  }
+
+  BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+  queue<BinaryTreeNode<int> *> pendingNodes;
+  pendingNodes.push(root);
+
+  while (pendingNodes.size() != 0) {
+    BinaryTreeNode<int> *front = pendingNodes.front();
+    pendingNodes.pop();
+    int leftData, rightData;
+    cin >> leftData >> rightData;
+    if (leftData != -1) {
+      BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftData);
+      pendingNodes.push(leftNode);
+    }
+
+    if (rightData != -1) {
+      BinaryTreeNode<int> *rightNode = new BinaryTreeNode<int>(rightData);
+      pendingNodes.push(rightNode);
+    }
+  }
+  return root;
+}
+
+int main() { BinaryTreeNode<int> *root = takeInput(); }
